6146c02cba78ec7aaf0f0bf3d704491774d3ab3a
chriskossdummy <38363533+chriskossdummy@users.noreply.github.com>
2018-06-11 15:50:39 -0700

    Update remote.ps1

--- remote.ps1
+++ remote.ps1
@@ -5,6 +5,8 @@ Start-Process winword.exe C:\Users\IEUser\Downloads\malicious_documents_samples\
 
 Add-Type -AssemblyName System.Windows.Forms
 $screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+Start-Sleep -Seconds 2
 [Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(1000,100)
+Start-Sleep -Seconds 2
 
 Write-Output "Goodbye World -- remote execution complete"
