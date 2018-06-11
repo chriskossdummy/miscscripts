85affe16535cdcb0cd7391bee192b4b56d8ab9e4
chriskossdummy <38363533+chriskossdummy@users.noreply.github.com>
2018-06-11 16:16:09 -0700

    Update remote.ps1

--- remote.ps1
+++ remote.ps1
@@ -3,7 +3,7 @@ Write-Output "Hello World! -- written remote script downloaded from github"
 Start-Process winword.exe C:\Users\IEUser\Downloads\malicious_documents_samples\malicious_document_macro_dridex\dridexDropper.doc
 
 Start-Sleep -Seconds 5
-.\\scripts\enable.ps1
+ps C:\Users\IEUser\scripts\enable.ps1
 
 Start-Sleep -Seconds 10
 
