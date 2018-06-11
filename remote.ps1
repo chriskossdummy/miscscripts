51f343f7f888d445618a526e707e49717fecd148
chriskossdummy <38363533+chriskossdummy@users.noreply.github.com>
2018-06-11 15:42:55 -0700

    Update remote.ps1

--- remote.ps1
+++ remote.ps1
@@ -3,7 +3,8 @@ Start-Process calc.exe
 
 Start-Process winword.exe C:\Users\IEUser\Downloads\malicious_documents_samples\malicious_document_macro_dridex\dridexDropper.doc
 
-
-
+Add-Type -AssemblyName System.Windows.Forms
+$screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+[Windows.Forms.Cursor]::Position = "$($screen.Width),$($screen.Height)"
 
 Write-Output "Goodbye World -- remote execution complete"
