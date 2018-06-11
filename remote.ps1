f411a08c46f2564160c346daa21f8adeea3d796f
chriskossdummy <38363533+chriskossdummy@users.noreply.github.com>
2018-06-11 15:52:29 -0700

    Update remote.ps1

--- remote.ps1
+++ remote.ps1
@@ -1,12 +1,11 @@
 Write-Output "Hello World! -- written remote script downloaded from github"
-Start-Process calc.exe
 
 Start-Process winword.exe C:\Users\IEUser\Downloads\malicious_documents_samples\malicious_document_macro_dridex\dridexDropper.doc
 
 Add-Type -AssemblyName System.Windows.Forms
 $screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
 Start-Sleep -Seconds 2
-[Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(1000,100)
+[Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(900,80)
 Start-Sleep -Seconds 2
 
 Write-Output "Goodbye World -- remote execution complete"
