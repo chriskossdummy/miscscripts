716eac28e8337faaa7d5481e4601741546a9781b
chriskossdummy <38363533+chriskossdummy@users.noreply.github.com>
2018-06-11 15:38:11 -0700

    Update remote.ps1

--- remote.ps1
+++ remote.ps1
@@ -1,6 +1,8 @@
 Write-Output "Hello World! -- written remote script downloaded from github"
 Start-Process calc.exe
 
+Start-Process winword.exe C:\Users\IEUser\Downloads\malicious_documents_samples\malicious_document_macro_dridex\dridexDropper.doc
+
 
 
 
